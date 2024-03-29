2bf332cc6f05f8b23bf4f0b19dd2ebd9685ebee9
vitorleal2244 <vitorleal9@hotmail.com>
2024-03-29 00:30:27 +0000

    [Cavalum][FE] - Fixed the header on scroll. Changed login link

--- src/app/shared/layout/tab-bar/tab-bar.component.ts
+++ src/app/shared/layout/tab-bar/tab-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-tab-bar',
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./tab-bar.component.scss']
 })
 export class TabBarComponent {
+  navFixed: boolean = false;
+  private scrollOffset: number = 70;
 
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    this.navFixed = (window.scrollY
+      || document.documentElement.scrollTop
+      || document.body.scrollTop || 0
+    ) > this.scrollOffset;
+  }
 }
