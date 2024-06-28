c860e8ee1e692cfadf6a731950406be2323b5fa9
Vitor Leal <vitorleal9@hotmail.com>
2024-06-28 10:40:17 +0100

    [FE][Cavalum] - Correction of design bugs on homepage

--- src/app/core/components/card/card.component.ts
+++ src/app/core/components/card/card.component.ts
@@ -1,14 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
-import { MdbModalService } from 'mdb-angular-ui-kit/modal';
 import { ButtonComponent } from '../button/button.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-card',
-    templateUrl: './card.component.html',
-    styleUrls: ['./card.component.scss'],
-    standalone: true,
-    imports: [ButtonComponent]
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss'],
+  standalone: true,
+  imports: [ButtonComponent, CommonModule],
 })
 export class CardComponent {
   @Input({ required: true }) title: string = '';
@@ -16,9 +16,7 @@ export class CardComponent {
   @Input() image: string = '';
   @Input() link: string = '';
 
-  constructor(
-    private _route: Router
-  ) {}
+  constructor(private _route: Router) {}
 
   public goToLink() {
     this._route.navigateByUrl(this.link);
