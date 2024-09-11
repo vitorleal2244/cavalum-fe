8cef9e5ec4be4c24b902c740deeaed2c77d2f8ad
vleal1997 <vitorandreleal@hotmail.com>
2024-09-11 21:58:22 +0100

    [Cavalum][FE] - Started trail detail page. Missing search comment and comments

--- src/app/modules/trails/detail-trail/detail-trail.component.ts
+++ src/app/modules/trails/detail-trail/detail-trail.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core'
-import { TrailsService } from '../trails.service'
-import { HttpParams } from '@angular/common/http'
 import { ActivatedRoute } from '@angular/router'
 import { Trail } from 'src/app/core/interfaces/trail.interface'
+import { MatIconModule } from '@angular/material/icon'
+import { MatChipsModule } from '@angular/material/chips'
+import { MatProgressBarModule } from '@angular/material/progress-bar'
+import { TrailsService } from '../trails.service'
 
 @Component({
   selector: 'app-detail-trail',
   templateUrl: './detail-trail.component.html',
   styleUrls: ['./detail-trail.component.scss'],
   standalone: true,
+  imports: [MatIconModule, MatChipsModule, MatProgressBarModule],
 })
 export class DetailTrailComponent implements OnInit {
   public trailId!: string | null
