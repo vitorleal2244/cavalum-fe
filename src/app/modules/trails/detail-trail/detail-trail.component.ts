6432cc7588a1c5d7be7e62349cb4ad17cd780d12
Vitor Leal <39217722+vitorleal2244@users.noreply.github.com>
2024-09-12 09:26:29 +0100

    [Cavalum][FE] - Finished comments and search input. Reviews and comments not visible

--- src/app/modules/trails/detail-trail/detail-trail.component.ts
+++ src/app/modules/trails/detail-trail/detail-trail.component.ts
@@ -4,23 +4,36 @@ import { Trail } from 'src/app/core/interfaces/trail.interface'
 import { MatIconModule } from '@angular/material/icon'
 import { MatChipsModule } from '@angular/material/chips'
 import { MatProgressBarModule } from '@angular/material/progress-bar'
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
 import { TrailsService } from '../trails.service'
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-detail-trail',
   templateUrl: './detail-trail.component.html',
   styleUrls: ['./detail-trail.component.scss'],
   standalone: true,
-  imports: [MatIconModule, MatChipsModule, MatProgressBarModule],
+  imports: [
+    MatIconModule,
+    MatChipsModule,
+    MatProgressBarModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+    CommonModule,
+  ],
 })
 export class DetailTrailComponent implements OnInit {
   public trailId!: string | null
   public trailInfo!: Trail
+  public commentsVisible: boolean = false;
 
   constructor(
     private readonly trailsService: TrailsService,
     private readonly activedRoute: ActivatedRoute
-  ) {}
+  ) { }
 
   ngOnInit() {
     this.trailId = this.activedRoute.snapshot.paramMap.get('id')
