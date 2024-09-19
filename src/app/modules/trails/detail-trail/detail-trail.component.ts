5fd89c55269812f3baa80de085503dfdd2adf2c9
vleal1997 <vitorandreleal@hotmail.com>
2024-09-19 18:32:39 +0100

    [Cavalum][FE] - Improvised trails list and trail detail pages

--- src/app/modules/trails/detail-trail/detail-trail.component.ts
+++ src/app/modules/trails/detail-trail/detail-trail.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core'
-import { ActivatedRoute } from '@angular/router'
+import { ActivatedRoute, RouterLink } from '@angular/router'
 import { Trail } from 'src/app/core/interfaces/trail.interface'
 import { MatIconModule } from '@angular/material/icon'
 import { MatChipsModule } from '@angular/material/chips'
 import { MatProgressBarModule } from '@angular/material/progress-bar'
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatCardModule } from '@angular/material/card'
 import { TrailsService } from '../trails.service'
 import { CommonModule } from '@angular/common'
 
@@ -23,17 +23,18 @@ import { CommonModule } from '@angular/common'
     MatFormFieldModule,
     MatCardModule,
     CommonModule,
+    RouterLink,
   ],
 })
 export class DetailTrailComponent implements OnInit {
   public trailId!: string | null
   public trailInfo!: Trail
-  public commentsVisible: boolean = false;
+  public commentsVisible: boolean = false
 
   constructor(
     private readonly trailsService: TrailsService,
     private readonly activedRoute: ActivatedRoute
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.trailId = this.activedRoute.snapshot.paramMap.get('id')
