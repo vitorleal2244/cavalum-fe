bc184bd4ff9e91343cc084db40ee2e3476fe78cf
vleal1997 <vitorandreleal@hotmail.com>
2024-09-23 11:56:35 +0100

    [Cavalum][FE] - Created docker file

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:19.5.0-alpine
+
+WORKDIR /usr/src/app
+
+COPY . /usr/src/app/
+
+RUN npm install -g @angular/cli
+
+RUN npm install --legacy-peer-deps
+
+CMD ["ng", "serve", "--host", "0.0.0.0"]
--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
-import { environment } from 'src/environments/environment';
-import { TranslateModule } from '@ngx-translate/core';
+/*
+ * File: footer.component.ts
+ * Project: Cavalum
+ * Created: Saturday, 7th September 2024 7:58:21 pm
+ * Last Modified: Monday, 23rd September 2024 11:44:58 am
+ * Copyright © 2024 Vitor Leal
+ */
 
+import { Component } from '@angular/core'
+import { environment } from 'src/environments/environment'
+import { TranslateModule } from '@ngx-translate/core'
+
+/**
+ * Footer component
+ *
+ * @class
+ * @exports
+ */
 @Component({
-    selector: 'app-footer',
-    templateUrl: './footer.component.html',
-    styleUrls: ['./footer.component.scss'],
-    standalone: true,
-    imports: [TranslateModule]
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+  standalone: true,
+  imports: [TranslateModule],
 })
 export class FooterComponent {
-  public website_name = environment.geral.name;
-  public present_year = new Date().getFullYear();
+  public website_name = environment.geral.name
+  public present_year = new Date().getFullYear()
 }
