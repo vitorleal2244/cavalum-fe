0dd39779d1679597c8faa3bb3c577a98f7351dcd
vleal1997 <vitorandreleal@hotmail.com>
2024-09-25 10:19:41 +0100

    [Cavalum][FE] - Build done

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  production: false,
+  geral: {
+    name: 'Cavalum',
+  },
+  auth: {
+    domain: '',
+    clientId: 'KdS51wf6iI0VZIDSlWWhnseIigpbxQ2J',
+    redirectUri: window.location.origin,
+  },
+  api: {
+    url: 'https://peaceful-coast-94321-b252a0ecc94b.herokuapp.com',
+    local: '/assets/temp_data/',
+  },
+  domain: 'https://www.cavalum.pt',
+  email: {
+    public_key: 'iWYIoALgm04O0f___',
+  },
+}
