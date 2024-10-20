e700ef71a096a0d197895824b03c38e0d0ae3c9b
vleal1997 <vitorandreleal@hotmail.com>
2024-10-20 20:17:36 +0100

    [Life manager][FE] - Improved news details page in terms of style

--- src/app/modules/about/about.component.ts
+++ src/app/modules/about/about.component.ts
@@ -1,20 +1,28 @@
-import { Component } from '@angular/core';
-import { GalleryComponent } from '../../core/components/gallery/gallery.component';
-import { ImageTextComponent } from '../../core/components/image-text/image-text.component';
-import { ImageViewerComponent } from '../../core/components/image-viewer/image-viewer.component';
-import { NgIf } from '@angular/common';
+/*
+ * File: about.component.ts
+ * Project: Cavalum
+ * Created: Saturday, 7th September 2024 7:58:21 pm
+ * Last Modified: Sunday, 20th October 2024 2:48:17 pm
+ * Copyright © 2024 Vitor Leal
+ */
+
+import { Component } from '@angular/core'
+import { GalleryComponent } from '../../core/components/gallery/gallery.component'
+import { ImageTextComponent } from '../../core/components/image-text/image-text.component'
+import { ImageViewerComponent } from '../../core/components/image-viewer/image-viewer.component'
+import { NgIf } from '@angular/common'
 
 @Component({
-    selector: 'app-about',
-    templateUrl: './about.component.html',
-    styleUrls: ['./about.component.scss'],
-    standalone: true,
-    imports: [NgIf, ImageViewerComponent, ImageTextComponent, GalleryComponent]
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss'],
+  standalone: true,
+  imports: [NgIf, ImageViewerComponent, ImageTextComponent, GalleryComponent],
 })
 export class AboutComponent {
-  public imageViewer: string = '';
+  public imageViewer: string = ''
 
-  photos = [
+  public photos: string[] = [
     '../../../assets/images/gallery/img1.jpg',
     '../../../assets/images/gallery/img2.JPG',
     '../../../assets/images/gallery/img3.JPG',
@@ -23,15 +31,23 @@ export class AboutComponent {
     '../../../assets/images/gallery/img7.jpg',
     '../../../assets/images/gallery/img8.jpg',
     '../../../assets/images/gallery/img9.jpg',
-  ];
+  ]
 
+  /**
+   * Get's the photo
+   *
+   * @param {any} photo
+   * @returns
+   *
+   * @memberof AboutComponent
+   */
   public getPhoto(photo: any): void {
-    this.imageViewer = photo;
+    this.imageViewer = photo
   }
 
   public closeImageViewer(isClosed: any) {
     if (isClosed) {
-      this.imageViewer = '';
+      this.imageViewer = ''
     }
   }
 }
